195deaafdc31935d6ac11e618cecd18b7682bc11
BankTQMA <minecraftforce1234@gmail.com>
2025-10-03 10:21:27 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const myInput = document.getElementById("myInput")
+
+function plus() {
+    myInput.value = Number(myInput.value) + 1
+}
+
+function minus() {
+    myInput.value = Number(myInput.value) - 1
+}
